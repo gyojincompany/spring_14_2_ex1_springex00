fd7654ba07d92d527fa4c8c6f67d2ed5f5430280
gyojincompany <luckyred@gmail.com>
2022-04-20 11:10:41 +0900

    데이터 객체(코맨드 객체) 이름 변경

---
+++ src/main/java/com/javagyojin/spring_14_2_ex/StudentInformation.java
@@ -0,0 +1,37 @@
+package com.javagyojin.spring_14_2_ex;
+
+public class StudentInformation {
+	
+	private String name;
+	private String age;
+	private String email;
+	private String address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+}
